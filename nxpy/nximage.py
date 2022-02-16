92f1816698a01237fae6936dd2cf6c8f3f24503e
Sebastian <SebastianDang@users.noreply.github.com>
2022-02-15 22:28:01 -0700

    fix: Unpack image as rgba8888

--- nxpy/nximage.py
+++ nxpy/nximage.py
@@ -1,5 +1,7 @@
 import lz4.block
 
+def unpack_data_to_rgba8888(data):
+    data[0::4], data[2::4] = data[2::4], data[0::4]
 
 class NXImage:
 
@@ -14,4 +16,5 @@ class NXImage:
         self.nxfile.file.seek(self.offset)
         compressed_size = int.from_bytes(self.nxfile.file.read(4), 'little')
         compressed_bytes = self.nxfile.file.read(compressed_size)
-        return lz4.block.decompress(compressed_bytes, self.width * self.height * 4, True)
+        data = lz4.block.decompress(compressed_bytes, self.width * self.height * 4, True)
+        return unpack_data_to_rgba8888(data)
